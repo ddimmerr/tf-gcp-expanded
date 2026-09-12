433e3e46d35d427543157be828b2097435ce9e87
Dmytro <98242309+ddimmerr@users.noreply.github.com>
2026-09-12 17:17:10 +0200

    Remove artifact registry resources from nonprod
    Removed Google Artifact Registry repository and IAM member resources for nonprod environment.

--- terraform/envs/nonprod/main.tf
+++ terraform/envs/nonprod/main.tf
@@ -32,19 +32,6 @@ module "nonprod_vm" {
   ssh_public_key = var.ssh_public_key
 }
 
-resource "google_artifact_registry_repository" "app_repo" {
-  location      = "us-central1"
-  repository_id = "app-repo"
-  format        = "DOCKER"
-}
-
-resource "google_artifact_registry_repository_iam_member" "nonprod_vm_reader" {
-  location   = google_artifact_registry_repository.app_repo.location
-  repository = google_artifact_registry_repository.app_repo.name
-  role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${module.nonprod_vm.service_account_email}"
-}
-
 output "nonprod_ip" {
   value = module.nonprod_vm.public_ip
 }
